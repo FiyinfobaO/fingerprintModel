4691b2089a3a2a37fc2dcd9061362617066253e6
Ogunkeye Fiyinfoba <fiyinfobaadenla@yahoo.com>
2020-09-27 23:48:49 +0100

    minor updates

--- piFingerprint.py
+++ piFingerprint.py
@@ -1,18 +1,4 @@
-# Copyright 2019 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# from tflite_runtime.interpreter import Interpreter
+from tflite_runtime.interpreter import Interpreter
 import numpy as np
 import argparse
 from PIL import Image
@@ -64,9 +50,3 @@ interpreter.invoke()
 predictions = interpreter.get_tensor(output_details[0]['index'])
 predicted_label = np.argmax(predictions)
 print(labels[predicted_label])
-
-# # Get indices of the top k results
-# top_k_indices = np.argsort(predictions)[::-1][:top_k_results]
-#
-# for i in range(top_k_results):
-#     print(labels[top_k_indices[i]], predictions[top_k_indices[i]] / 255.0)
